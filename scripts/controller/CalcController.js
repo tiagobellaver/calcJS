914f816bba9b558482e1e345fe9198fc1e8916b6
Tiago F. Bellaver <tfbellaver@hotmail.com>
2020-09-20 13:21:59 -0300

    Add event

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -10,6 +10,7 @@ class CalcController {
 
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -22,6 +23,22 @@ class CalcController {
             this.setDisplayDateTime();
 
         }, 1000);
+
+    }
+
+    initButtonsEvents() {
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=> {
+            
+            btn.addEventListener('click', e => {
+
+                console.log(btn.className.baseVal.replace("btn-",""));
+
+            });
+
+        });
 
     }
 
